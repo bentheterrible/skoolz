5e5995a6847e6fb7c1cbbe9bf241c8ebbabbea40
ben <bfeistmann@gmail.com>
2014-10-24 15:08:56 -0400

    nested for-loop

---
+++ comp_org/lec/meow.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	int row, col;
+	double var;
+	for(row = 1; row <= 20; row++) {
+		for(col = 1; col <= 12; col++) {
+
+			fscanf(ifp, "input.txt", &var);
+			printf("%d\t", var);
+		}	
+		printf("\n");
+	}	
+}
